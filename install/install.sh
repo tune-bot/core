6b4f057104737f00243123dd7277eed16edfc49f
cbeimers113 <chrisbeimers113@gmail.com>
2023-07-25 20:41:26 -0400

    Update var directory

--- install/install.sh
+++ install/install.sh
@@ -14,7 +14,7 @@ chmod a+rx bin/download
 
 bin/download -U
 
-source infrastructure/database.env
+source vars/database.env
 sed -i "s|DB_USER|$DB_USER|g" database/install/create.sql
 sed -i "s|DB_PASS|$DB_PASS|g" database/install/create.sql
 sed -i "s|DB_HOST|$DB_HOST|g" database/install/create.sql
@@ -27,6 +27,6 @@ mysql --defaults-extra-file=/etc/mysql/debian.cnf < database/install/create.sql
 service mysql stop
 
 echo "#!/bin/bash" > bin/database
-echo "source infrastructure/database.env" >> bin/database
+echo "source vars/database.env" >> bin/database
 echo "service mysql start" >> bin/database
 chmod a+rx bin/database
